f9f32f09a7e7e90bb93dd99664ba14c2a96896b8
Tan Loc Nguyen <tanggs00@hs-esslingen.de>
2017-03-29 20:02:46 +0200

    Modified quadcopter_dynamics.h

--- cpp-source/quadcopter_dynamics.h
+++ cpp-source/quadcopter_dynamics.h
@@ -1,6 +1,7 @@
-// This is start of the header guard.  ADD_H can be any unique name.  By convention, we use the name of the header file.
-#ifndef QUADCOPTERDYNAMICS_H
-#define QUADCOPTERDYNAMICS_H
+// This file is not yet finish. The dynamics is needed for calculating different parameters of the quadcopter.
+// This is start of the header guard. By convention, we use the name of the header file.
+#ifndef QUADCOPTER_DYNAMICS_H
+#define QUADCOPTER_DYNAMICS_H
 
 #include <math.h>
 #include <windows.h>
@@ -15,9 +16,20 @@ class QuadcopterDynamics{
 		static float acc_xyz[3];	
 		//calculation of x, y, z acceleration
 	}
-			
+	
+	float * calculateXYZVelocity(){
+	    static float velo_xyz[3];
+	    //For calculating xyz velocities
+	}
+	
+	float * calculateXYZPosition(){
+	    static float pos_xyz[3];
+	    //For calculating xyz positions
+	}
+    
 	float * calculateOmegaAcceleration(){
-		static float omega_acc[3];	//For the 
+		static float omega_acc[3];	
+		//For calculating the angular acceleration of the quadcopter 
 	}
 };
  
