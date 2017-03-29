2b836f4acfd78c1e6083fc7307f7f5421444fff9
Tan Loc Nguyen <tanggs00@hs-esslingen.de>
2017-03-29 20:52:45 +0200

    unitTest.cpp is just for testing classes and functions used in this project

---
+++ cpp-source/unitTest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <windows.h>
+#include <quadcopter_dynamics.h>
+#include <Postrack.h>
+#include <UDPController.h>
+
+void main(){
+    // Init all the classes that needed to be tested here
+    // Test Logic
+}
