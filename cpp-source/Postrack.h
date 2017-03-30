07452a590d878a87e6113e70a4e6a67f72c1b386
maligs00 <maligs00@hs.esslingen.de>
2017-03-30 16:59:00 +0200

    Added comments

--- cpp-source/Postrack.h
+++ cpp-source/Postrack.h
@@ -4,41 +4,61 @@
 
 struct KinematicData;
 
+// xml_summary_tag.cpp
+// compile with: /LD /clr /doc
+// post-build command: xdcmake xml_summary_tag.dll
+
+/// Class Postrack can track a Object based on acceleration and timestamps, or based on the time the next update will arrive. 
+
 class Postrack
 {
 public:
-	
+		
+	/// <summary>Postrack Constructor.
+	/// </summary>
 	Postrack();
 	~Postrack();
 
+	/// Creates a new point (KinematicData) in the objects path and calculates the new position. Point pets pushed to path then.
 	bool updatePos(double acc);
 	
+	/// Returns the whole path in KinematicData objects
 	std::vector<KinematicData> getPath();
 
+	/// Prints the whole path to std::cout
 	bool printPath();
 
 private:
 	
+	/// Path of the object to track
 	std::vector<KinematicData> path;
+
+	/// Start time when te class is created
 	std::chrono::steady_clock::time_point start;
 
+	/// Help function that will calculate the difference of two time points and manipulate it in the KinematicData object
 	bool calcTime(KinematicData &h);
+	
+	/// Help function that will calculate the new position based on acceleration and time elapsed since last update and manipulates it in the KinematicData object
 	bool calcPos(KinematicData &h);
 
+	/// Help function that will calculate the velocity based on acceleration, initial velocity and time
 	double getVel(double time, double initVel, double acc);
+
+	/// Help function that will calculate the movement based on velocity and time
 	double getMov(double time, double actVel);
 
 };
 
 struct KinematicData
 {
-	int    typ;         // Achstyp translatorisch (TRANS_X, TRANS_Y, TRANS_Z) oder rotatorisch (ROT_X, ROT_Y, ROT_Z)
+	int    typ;				// not in use atm - Achstyp translatorisch (TRANS_X, TRANS_Y, TRANS_Z) oder rotatorisch (ROT_X, ROT_Y, ROT_Z)
 	double trans_x = 0;     // Translation um trans_x in mm
-	double trans_y = 0;     // Translation um trans_y in mm
-	double trans_z = 0;     // Translation um trans_z in mm
-	double rot_x = 0;       // Rotation um rot_x in rad
-	double rot_y = 0;       // Rotation um rot_y in rad
-	double rot_z = 0;       // Rotation um rot_z in rad
+	double trans_y = 0;     // not in use atm - Translation um trans_y in mm
+	double trans_z = 0;     // not in use atm - Translation um trans_z in mm
+	double rot_x = 0;       // not in use atm - Rotation um rot_x in rad
+	double rot_y = 0;       // not in use atm - Rotation um rot_y in rad
+	double rot_z = 0;       // not in use atm - Rotation um rot_z in rad
 
 	std::chrono::steady_clock::time_point oldTime;
 	std::chrono::steady_clock::time_point newtime;
@@ -48,6 +68,6 @@ struct KinematicData
 	double initVel = 0;		// Velocity in mm/s
 	double actVel = 0;		// Velocity in mm/s
 	double pos_x = 0;		// Position on x-axis in mm 
-	double pos_y = 0;		// Position on y-axis in mm
-	double pos_z = 0;		// Position on z-axis in mm
+	double pos_y = 0;		// not in use atm - Position on y-axis in mm
+	double pos_z = 0;		// not in use atm - Position on z-axis in mm
 };
--- cpp-source/main.cpp
+++ cpp-source/main.cpp
@@ -9,7 +9,6 @@
 #include <iterator>
 #include <algorithm>
 
-
 #include "Postrack.h"
 #include "UDPConroller.h"
 #include "UDPClient.h"
@@ -18,35 +17,50 @@
 
 void main(){
 
+	// create io_services for network communication
 	boost::asio::io_service io_service;
 
+	// add the services to a second thread
 	boost::thread bt(boost::bind(&boost::asio::io_service::run, &io_service));
 
+	// create UDP Client on port 13
 	UDPClient client(io_service, "localhost", "13");
+	
+	// create UDP Server
 	UDPController server(io_service);
 
+	// start Network communication services
 	io_service.run();
 
+	// wait
 	std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
+	
+	// only testing the client
 	client.send("Hello, World!");
 
+	// create new Postrack class
 	Postrack* h = new Postrack();
 
+	// simulate first package arriving
 	h->updatePos(10);
-	std::this_thread::sleep_for(std::chrono::microseconds(10));
-	h->updatePos(300);
-	std::this_thread::sleep_for(std::chrono::milliseconds(1));
-	h->updatePos(40);
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(2));
+	// wait for some time...
+	std::this_thread::sleep_for(std::chrono::milliseconds(30));
+
+	// simulate second package arriving
+	h->updatePos(300);
+	std::this_thread::sleep_for(std::chrono::milliseconds(40));
+
+	h->updatePos(40);
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
 	h->updatePos(-100);
-	std::this_thread::sleep_for(std::chrono::microseconds(100));
+	std::this_thread::sleep_for(std::chrono::microseconds(600));
 	h->updatePos(-50);
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	h->updatePos(-500);
+
 	std::this_thread::sleep_for(std::chrono::milliseconds(5));
 	h->updatePos(320);
 
@@ -54,6 +68,7 @@ void main(){
 
 	char x;
 
+	// keeps te window open till you press any key and enter
 	std::cin >> x;
 
 	io_service.stop();
