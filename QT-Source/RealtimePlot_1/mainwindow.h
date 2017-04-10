3194ce7d7b538d525938c9a04ed2cad45ba85eb9
Abraham Barrera Villalpando <abbags01@hs-esslingen.de>
2017-04-10 17:55:57 +0200

    QT application for tracking QuadCopter´s position. Version 1.0 Only 2 Axis available

---
+++ QT-Source/RealtimePlot_1/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    void addPoint(double x, double y);
+    void clearData();
+    void plot();
+
+private slots:
+    void on_btn_add_clicked();
+
+    void on_btn_clear_clicked();
+
+private:
+    Ui::MainWindow *ui;
+
+    QVector<double> qv_x, qv_y;
+};
+
+#endif // MAINWINDOW_H
