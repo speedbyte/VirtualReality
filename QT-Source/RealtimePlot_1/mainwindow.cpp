061be6f93e4339ed4956949c98f3be690d2bf502
Abraham Barrera Villalpando <abbags01@hs-esslingen.de>
2017-04-24 17:42:55 +0200

    Ploting position from a vector. Vector currently generated locally. Vector to be updated with UDP packages.

--- QT-Source/RealtimePlot_1/mainwindow.cpp
+++ QT-Source/RealtimePlot_1/mainwindow.cpp
@@ -6,9 +6,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->plot->addGraph();
-    ui->plot->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
-    ui->plot->graph(0)->setLineStyle(QCPGraph::lsNone);
+    ui->Plot_Altitude->addGraph();
+    ui->Plot_Altitude->graph(0)->setScatterStyle(QCPScatterStyle::ssCross);
+    ui->Plot_Altitude->graph(0)->setLineStyle(QCPGraph::lsNone);
+    //ui->plot->graph(0)->setValueAxis(QCPAxis);
+
+    QString GraphName = "Position";
+    ui->Plot_Altitude->graph(0)->setName(GraphName);
+
+   // ui->plot->graph(0)->keyAxis->label(GraphName);
+
+
+
+    ui->Plot_xyPos->addGraph();
+    ui->Plot_xyPos->graph(0)->setScatterStyle(QCPScatterStyle::ssCircle);
+    ui->Plot_xyPos->graph(0)->setLineStyle(QCPGraph::lsNone);
+
 
 }
 
@@ -17,33 +30,67 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::addPoint(double x, double y)
-{
-    qv_x.append(x);
-    qv_y.append(y);
-}
+
 
 void MainWindow::clearData()
 {
-    qv_x.clear();
-    qv_y.clear();
+    vTime.clear();
+    vAltitude.clear();
+    vCoord_x.clear();
+    vCoord_y.clear();
 }
 
 void MainWindow::plot()
 {
-    ui->plot->graph(0)->setData(qv_x,qv_y);
-    ui->plot->replot();
-    ui->plot->update();
+    ui->Plot_Altitude->graph(0)->setData(vTime,vAltitude);
+    ui->Plot_xyPos->graph(0)->setData(vCoord_x,vCoord_y);
+
+    ui->Plot_Altitude->replot();
+    ui->Plot_xyPos->replot();
+    ui->Plot_Altitude->update();
+    ui->Plot_xyPos->update();
 }
 
-void MainWindow::on_btn_add_clicked()
+
+void MainWindow::on_btn_PrPath_clicked()
 {
-    addPoint(ui->bx_x->value(),ui->bx_y->value());
+    clearData();
+    for (int i=0; i<25; ++i)
+    {
+    vCoord_x.append(i*0.2);
+    vCoord_y.append(i*i*0.01);
+
+    vTime.append(i*0.2);//
+    vAltitude.append((i*i*0.002)+1);
+
+    QThread::msleep(100);
     plot();
+
+    }
 }
 
 void MainWindow::on_btn_clear_clicked()
 {
     clearData();
     plot();
+
+}
+
+void MainWindow::on_btn_Refresh_clicked()
+{
+clearData();
+ for (int i=0; i<25; ++i)
+ {
+ vCoord_x.append(i*0.2);
+
+  vCoord_y.append(qExp(i*0.01));
+
+ vTime.append(i*0.2);//
+ vAltitude.append((i*i*-(0.002))+2);
+
+ QThread::msleep(100);
+ plot();
+
+ }
+
 }
