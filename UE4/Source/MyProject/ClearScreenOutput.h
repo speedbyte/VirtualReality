63bbf889659e5b211176db40171eecf711ed4230
Markus Lindner <maligs00@hs.esslingen.de>
2017-07-26 19:14:09 +0200

    Update UE4

---
+++ UE4/Source/MyProject/ClearScreenOutput.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ClearScreenOutput.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UClearScreenOutput : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
